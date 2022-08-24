964fe917d167808db6d52a68fbaccb8116661060
DrRuski <ilia_e@hotmail.com>
2022-08-24 13:21:30 +0200

    commit

---
+++ js/question-2.js
@@ -0,0 +1,27 @@
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=b560cfe448e24c49aaa9380d90353667"
+const library = document.querySelector(".gamesLibrary");
+
+
+
+async function gameLibrary(){
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log(result);
+
+        
+        library.innerHTML = "";
+
+        for (let i = 0; i < result.length; i++){
+            
+            
+        }
+    }
+    catch(error) {
+        console.log("Something Went Wrong", error);
+    }
+    
+}
+
+gameLibrary();
